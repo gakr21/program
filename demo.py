70d8310c3d2caa74dfe1a75e167c5538e3c4552e
Ashok <ashokreddy4331@gmail.com>
2023-02-28 09:54:02 +0530

    first commit

---
+++ demo.py
@@ -0,0 +1,55 @@
+https://github.com/gakr21/program.gin=5
+#
+# for i in range(1,n+1):
+#    for j in range(1,n+1):
+#       print(i,end="")
+#    print()
+
+
+n=int(input("enter"))
+for i in range(1,n+1):
+   for j in range(1,n+1):
+      print(i,end="")
+   print()
+
+# n=int(input("enter "))
+# for i in range(1,n+1):
+#    for j in range(5,0,-1):
+#       print(j,end="")
+#    print()
+
+# n=int(input("enter"))
+# for i in range(5,0,-1):
+#    for j in range(1,n+1):
+#        print(i,end="")
+#    print()
+
+
+# n=int(input("enter"))
+# for i in range(1,n+1):
+#    for j in range(1,i+1):
+#       print(j,end="")
+#    print()
+
+# n=int(input("enter"))
+# for i in range(1,n+1):
+#    print(" *"*i)
+
+# n=5
+# for i in range(1,n+1):
+#    print(((n-i)*" ")+" *"*i)
+# print()
+
+# n=int(input("enter a number"))
+# for i in range(1,n+1):
+#    print((n-i)*" ","*"*i)
+
+# for i in range(1,n+1):
+#    print((n-i)*" ","* "*i)
+   # print("*"*i,(n-i)*" ")
+
+
+
+
+
+
